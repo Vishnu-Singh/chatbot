d9d223e6102721dd8e94e67289bfdf2ca82b4fd9
Vishnu Singh <vishnusingh1847@gmail.com>
2023-04-13 08:38:35 +0530

    pickel data model created

--- dialogbot/models.py
+++ dialogbot/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from uuid import uuid4
 # Create your models here.
+class PickelData(models.Model):
+    uuid = models.CharField(primary_key=True,default=uuid4,max_length=50)
+    dependent_variables = models.BinaryField()
+    independent_variables = models.BinaryField()
+    checkpoint_state = models.BinaryField()

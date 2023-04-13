061bc5967996005ea7f423191632da1998ec742c
Vishnu Singh <vishnusingh1847@gmail.com>
2023-04-13 09:14:15 +0530

    model updated

--- dialogbot/models.py
+++ dialogbot/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from uuid import uuid4
+import os
+
 # Create your models here.
 class PickelData(models.Model):
     uuid = models.CharField(primary_key=True,default=uuid4,max_length=50)
-    dependent_variables = models.BinaryField()
-    independent_variables = models.BinaryField()
-    checkpoint_state = models.BinaryField()
+    dependent_variables = models.FileField(upload_to="./checkpoints/")
+    independent_variables = models.FileField(upload_to="./checkpoints/")
+    checkpoint_state = models.FileField(upload_to="./checkpoints/")

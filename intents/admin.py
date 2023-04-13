1c43266b37375c8295fdd383fc1b68f26d5fa7f5
Vishnu Singh <vishnusingh1847@gmail.com>
2023-04-13 08:18:39 +0530

    intent app register to superadmin

--- intents/admin.py
+++ intents/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import AnswersPattern,QuestionPattern,Intent
 # Register your models here.
+admin.site.register(AnswersPattern)
+admin.site.register(QuestionPattern)
+admin.site.register(Intent)
